b11b78057a0936e187eeee8a4e361be2717100b5
paulo-robson <121298894+paulo-robson@users.noreply.github.com>
2023-02-07 12:19:56 -0300

    Add files via upload

--- avl.py
+++ avl.py
@@ -13,17 +13,17 @@ def criar_arquivo(aeronave, efeito_solo):
         arq.write('0.0\n')          # Mach
         arq.write('0 1 %.2f\n' % -aeronave.hw) # IYsym   IZsym   Zsym
         arq.write('%.2f %.2f %.2f\n' % (aeronave.Sw, aeronave.cw, aeronave.bw)) # Sref    Cref    Bref
-        arq.write('%.3f 0.0 0.0\n' % (aeronave.xcg)) # Xref    Yref    Zref
+        arq.write('%.3f 0.0 -0.08\n' % (aeronave.xcg)) # Xref    Yref    Zref (Variei o Zcg -Paulo Robson-)
     else:
         arq = open(caminho_geometrias + aeronave.nome + '.avl', 'w')
         arq.write(aeronave.nome + '\n')
         arq.write('0.0\n')          # Mach
         arq.write('0 0 0.0\n') # IYsym   IZsym   Zsym
         arq.write('%.2f %.2f %.2f\n' % (aeronave.Sw, aeronave.cw, aeronave.bw)) # Sref    Cref    Bref
-        arq.write('%.3f 0.0 0.0\n' % (aeronave.xcg)) # Xref    Yref    Zref
+        arq.write('%.3f 0.0 -0.08\n' % (aeronave.xcg)) # Xref    Yref    Zref (Variei o Zcg -Paulo Robson-)
     arq.write("\nSURFACE\n")
     arq.write("Asa\n")
-    arq.write("8 1.0 12 1.0\n") # Discretização
+    arq.write("12 1.0 30 1.0\n") # Discretização
     arq.write("YDUPLICATE\n0.0\n")
     arq.write("ANGLE\n%.0f\n" % aeronave.iw)
     arq.write("COMPONENT\n1\n")
